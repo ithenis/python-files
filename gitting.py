3ad70f61245ff5b0c0dc2226e4907286fe23e728
Nistea <elt29493@diamtr312.diamond.ac.uk>
2017-03-01 10:05:12 +0000

    first commit

--- gitting.py
+++ gitting.py
@@ -9,7 +9,7 @@ s('echo "This is my new repo" > README.txt')
 s('git add .') 
 s('git commit -m "first commit"')
     
-repo = "python-course"
+repo = "python-files"
 username = "ithenis"
 password = raw_input("Enter password: ")        # diamond1
 cmd = "git remote add origin https://{0}:{1}@github.com/{0}/{2}.git".format(username, password, repo)
